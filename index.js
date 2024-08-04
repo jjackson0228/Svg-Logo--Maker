43f51a71cb196e48110b9ff08c0ee8341c4ba0f7
James Jackson <jjackson0228@gmail.com>
2024-08-04 14:34:49 -0400

    condensed the index.js

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
-// Import necessary modules
-const LogoMaker = require("./LogoMaker");
+const shape = require("./lib/shapes.js");
 
-// Create an instance of LogoMaker
-const logoMaker = new LogoMaker();
+const shapes = new shape();
 
-// Handle user input (text, text color, shape, shape color)
-// You can use prompts or any other method to gather user input
-
-// Generate SVG file based on user input
-logoMaker.generateSVG();
-
-// Print output
-console.log("Generated logo.svg");
+shapes.run();
